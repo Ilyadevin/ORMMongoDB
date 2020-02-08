9b4c6a82c210aebbaa6252e0150387f5f9d1dce6
Ilya Erlingas <Aligruit@gmail.com>
2020-02-08 14:46:45 +0300

    #correction08022019

--- ORM_MongoDB.py
+++ ORM_MongoDB.py
@@ -2,53 +2,52 @@ import csv
 import re
 
 import pymongo
-from pymongo import Connection
-connection = Connection()
-db = connection.db_hw_mongo
+from mongoengine import connect
+
+db = connect("db_hw_mongo")
 
 
-def read_data(csv_file, db):
+def read_data(csv_file, database):
     """
     Загрузить данные в бд из CSV-файла
     """
-    coll = db.artists
+    collecion = database['artists']
     with open(csv_file, encoding='utf8') as csvfile:
         # прочитать файл с данными и записать в коллекцию
+        csvfiles_int = [map(int, row) for row in csv.reader(csvfile, delimiter=',')]
         reader = csv.DictReader(csvfile)
-        coll.save({reader})
-        for artist in coll.find():
-            print(artist)
+        collecion.insert_one({'Исполнитель': reader, 'Цена': reader, 'Место': reader, 'Дата': reader})
 
 
 read_data('artists.csv', db)
 
 
-def find_cheapest(db):
+def find_cheapest(database):
     """
     Отсортировать билеты из базы по возрастанию цены
     Документация: https://docs.mongodb.com/manual/reference/method/cursor.sort/
     """
-    print(db.posts.count())
-    for post in db.posts.find({}, {'Цена': 1}).sort():
-        print(post)
+    database.artists.find().sort({'Цена': 1})
 
 
 find_cheapest(db)
 
 
-def find_by_name(name, db):
+def find_by_name(name, database):
     """
     Найти билеты по имени исполнителя (в том числе – по подстроке, например "Seconds to"),
     и вернуть их по возрастанию цены
     """
-    coll = db.artists
     regex = re.compile(r'(^\w*[\s-]\w*|^\w*)')
-    sorted_coll = coll.find({'name': regex})
-    for names in sorted_coll({}).sort([('name', 1), 'Цена']):
-        if name in names:
-            print(name)
+    with open('artists.csv', encoding='utf8') as f:
+        if name in regex:
+            name_finded = name
+            database.artists.find({'name': name_finded}).sort({'Цена': 1})
+        else:
+            pass
 
 
 find_by_name('Seconds', db)
+
 if __name__ == '__main__':
     pass
