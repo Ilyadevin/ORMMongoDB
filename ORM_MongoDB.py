c5534b2ca930455a7c2a5e749ed7312c92678840
Ilya Erlingas <Aligruit@gmail.com>
2020-02-10 01:53:58 +0300

    #correcting10022020#

--- ORM_MongoDB.py
+++ ORM_MongoDB.py
@@ -1,7 +1,8 @@
 import csv
 from pymongo import MongoClient
 import re
-
+from datetime import datetime
+import time
 connection = MongoClient()
 db = connection.db_hw_mongo
 
@@ -11,8 +12,11 @@ def read_data(csv_file, database):
     with open(csv_file, 'r', encoding='utf8') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
-            row_dict = {'Исполнитель': row[0], "Цена": int(row[1]), "Место": row[2], "Дата": float(row[3])}
-            database.artist.insert_many(row_dict)
+            row_dict = {'Исполнитель': row[0],
+                        "Цена": int(row[1]),
+                        "Место": row[2],
+                        "Дата": datetime.strptime(row[3], '%d.%m')}
+            database.artist.insert_one(row_dict)
 
 
 read_data('artists.csv', db)
@@ -20,8 +24,7 @@ read_data('artists.csv', db)
 
 def find_cheapest(database):
 
-    database.artist.find()
-    database.artist.find().sort([('Цена', 1)])
+    return database.artist.find().sort([('Цена', 1)])
 
 
 find_cheapest(db)
@@ -29,8 +32,9 @@ find_cheapest(db)
 
 def find_by_name(name, database):
 
-    regex = re.compile(r'(^\w*[\s-]\w*|^\w*)')
-    return database.artist.find({'Исполнитель': {regex: name}})
+    pattern = re.compile(r'(^\w*[\s-]\w*|^\w*)')
+    regex = pattern.match(name)
+    return database.artist.find({'Исполнитель': regex})
 
 
 find_by_name('Seconds', db)
